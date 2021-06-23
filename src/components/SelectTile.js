3bb20bbabbf59f059d5b676c6653a8539c77bb35
ethancroft <ethan.croft@geekology.ltd.uk>
2021-06-23 16:02:56 +0100

    Makes buttons glow

--- src/components/SelectTile.js
+++ src/components/SelectTile.js
@@ -5,6 +5,26 @@ class SelectTile extends React.Component {
         super(props);
 
         this.onIngredientClick = this.onIngredientClick.bind(this);
+        this.onMouseLeaveTile = this.onMouseLeaveTile.bind(this);
+        this.createBackgroundEffect = this.createBackgroundEffect.bind(this);
+        this.ref = React.createRef();
+    }
+
+    onMouseLeaveTile() {
+        this.ref.current.style.background = '';
+    }
+
+    createBackgroundEffect(e) {
+        let tileCurrentPos = this.ref.current.getBoundingClientRect();
+        let { width, height } = tileCurrentPos;
+
+        let mouseDistanceFromLeft = e.nativeEvent.offsetX;
+        let mouseDistanceFromTop = e.nativeEvent.offsetY;
+
+        let percentLeft = Math.floor((mouseDistanceFromLeft / width * 100));
+        let percentTop = Math.floor((mouseDistanceFromTop / height * 100));
+
+        this.ref.current.style.background = `radial-gradient(ellipse at ${percentLeft}% ${percentTop}%, #ffda8b 5%, #f7c455 100%)`;
     }
 
     onIngredientClick() {
@@ -14,9 +34,12 @@ class SelectTile extends React.Component {
     render() {
         return (
             <button
+                onMouseMove={this.createBackgroundEffect}
+                onMouseLeave={this.onMouseLeaveTile}
                 onClick={this.onIngredientClick}
                 ingredient-id={this.props.ingredientId}
                 className={this.props.className}
+                ref={this.ref}
             >
                 {this.props.ingredientName}
             </button>

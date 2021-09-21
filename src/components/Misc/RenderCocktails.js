e927d46eb80f6651e1806737e056e4ff51c047b3
= <=>
2021-09-21 17:05:01 +0100

    Moves misc components into misc folder

---
+++ src/components/Misc/RenderCocktails.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SimpleList from "../SimpleList/SimpleList";
+
+const RenderCocktails = (props) => {
+    return <SimpleList listItems={props.matchedCocktails}></SimpleList>;
+};
+
+export default RenderCocktails;
